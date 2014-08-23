d2769b4080c66cdf20c71aee574ae91ab688b9ed
Camille <Kamiru_@live.com>
2014-08-23 23:30:18 +0200

    Update todo_button.sh

--- todo/todo_button.sh
+++ todo/todo_button.sh
@@ -4,4 +4,5 @@
 pkill -f "dzen2 -p -dock -title-name todo_button"*
 
 # Spawn music button
-bash /home/onodera/.scripts/todo/todo_content.sh | dzen2 -p -dock -title-name todo_button -fn -gohu-gohufont-medium-r-normal--14-100-100-100-c-80-iso10646-1 -ta c -w 162 -h 34 -e 'button1=exec:urxvt -e ne /home/onodera/.scripts/todo/text;'
+sleep 0.2
+bash /home/onodera/.scripts/todo/todo_content.sh | exec dzen2 -p -dock -title-name todo_button -fn -gohu-gohufont-medium-r-normal--14-100-100-100-c-80-iso10646-1 -ta c -w 162 -h 34 -e 'button1=exec:urxvt -e ne /home/onodera/.scripts/todo/text;' & disown
