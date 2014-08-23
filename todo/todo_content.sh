09c7b33eea13613ceb219dcbd57346be526b1a49
Camille <Kamiru_@live.com>
2014-08-23 23:29:24 +0200

    Update todo_content.sh

--- todo/todo_content.sh
+++ todo/todo_content.sh
@@ -5,7 +5,7 @@ while true; do
 	todo=$(cat /home/onodera/.scripts/todo/text | sed '/^\s*$/d' | wc -l)
 
 	# Send content to todo_button.sh
-	echo "^fg(#E8DFD6)$todo things todo  ^fg(#FF99A1)^i(/home/onodera/.scripts/todo/icon.xbm)"
+	echo "^fg(#E8DFD6)$todo reminders  ^fg(#FF99A1)^i(/home/onodera/.scripts/todo/icon.xbm)"
 
-	sleep 3
+	sleep 9
 done
