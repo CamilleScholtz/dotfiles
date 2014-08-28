70020d68cc7dcc54d2314e3c6ea1682f4167aa32
onodera-punpun <kamiru_@live.com>
2014-08-28 20:37:37 +0200

    yup..

---
+++ notify/capture_wscrot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Combine all the variables into a single output and send this to notify.sh
+echo "Window has been scrotted" > /home/onodera/.scripts/notify/text
+
+# Tell notify.sh that it needs to display a notication
+exec bash /home/onodera/.scripts/notify/notify.sh & disown
