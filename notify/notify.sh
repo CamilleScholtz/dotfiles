1b626f6e4752d241083c7b867cdfc2fc9d572654
onodera-punpun <kamiru_@live.com>
2014-09-06 15:22:22 +0200

    Perfected notify, it is now pretty much perfect

--- notify/notify.sh
+++ notify/notify.sh
@@ -13,33 +13,33 @@ width=$(expr $(echo $text | pcregrep -o "[^\x00-\x7F]" | wc -m) / 3 + $count - 1
 # Get number of existing notifications
 exist=$(pgrep -f "urxvt -name notification" | wc -w)
 
-# Spawn notifications
+# Get oldest notification numbers
+last0=$(ps -p $(pgrep -f -o "urxvt -name notification") -o args | grep -o "[0-9]" | head -1)
+last1=$(expr $last0 + 1)
+last2=$(expr $last0 + 2)
+last3=$(expr $last0 + 3)
+last4=$(expr $last0 + 4)
+
+# Move old notifications
 if [[ $exist -eq 1 ]]; then
-	wmctrl -x -r notification0 -e "0,-1,84,-1,-1"
+	wmctrl -x -r notification$last0 -e "0,-1,84,-1,-1"
 elif [[ $exist -eq 2 ]]; then
-	wmctrl -x -r notification0 -e "0,-1,134,-1,-1"
-	wmctrl -x -r notification1 -e "0,-1,84,-1,-1"
+	wmctrl -x -r notification$last0 -e "0,-1,134,-1,-1"
+	wmctrl -x -r notification$last1 -e "0,-1,84,-1,-1"
 elif [[ $exist -eq 3 ]]; then
-	wmctrl -x -r notification0 -e "0,-1,184,-1,-1"
-	wmctrl -x -r notification1 -e "0,-1,134,-1,-1"
-	wmctrl -x -r notification2 -e "0,-1,84,-1,-1"
+	wmctrl -x -r notification$last0 -e "0,-1,184,-1,-1"
+	wmctrl -x -r notification$last1 -e "0,-1,134,-1,-1"
+	wmctrl -x -r notification$last2 -e "0,-1,84,-1,-1"
 elif [[ $exist -eq 4 ]]; then
-	last=$(ps -p $(pgrep -f -o "urxvt -name notification") -o args | grep -o "[0-9]" | head -1)
+	pkill -f "urxvt -name notification$last0"
 
-	pkill -f "urxvt -name notification$last"
+	wmctrl -x -r notification$last1 -e "0,-1,184,-1,-1"
+	wmctrl -x -r notification$last2 -e "0,-1,134,-1,-1"
+	wmctrl -x -r notification$last3 -e "0,-1,84,-1,-1"
 
-	plus1=$(expr $last + 1)
-	plus2=$(expr $last + 2)
-	plus3=$(expr $last + 3)
-	plus4=$(expr $last + 4)
-
-	wmctrl -x -r notification$plus1 -e "0,-1,184,-1,-1"
-	wmctrl -x -r notification$plus2 -e "0,-1,134,-1,-1"
-	wmctrl -x -r notification$plus3 -e "0,-1,84,-1,-1"
-
-	urxvt -name notification$plus4 -geometry "$width"x1 -internalBorder 10 -hold -cursorUnderline -cursorColor $color -cursorColor2 $color -e echo -n "$text" & disown
+	urxvt -name notification$last4 -geometry "$width"x1 -internalBorder 10 -hold -cursorUnderline -cursorColor $color -cursorColor2 $color -e echo -n "$text" & disown
 	sleep 5
-	pkill -f "urxvt -name notification$plus4"
+	pkill -f "urxvt -name notification$last4"
 	exit
 fi 
 
